7f6d2ac65d99a9f83bb5d471004cd72db8fa7de2
RIFANASHERIN <67692764+RIFANASHERIN@users.noreply.github.com>
2022-11-11 11:59:50 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ const PANTRY_ID = 'e76bc9c7-92f6-4afe-b546-9d1865ea0fbe';
 
 const messages = document.querySelector('#messages');
 const in1 = document.querySelector('#in1');
+var data = [];
 
+const messageCard = (user, message) => `
+<div class="flex w-full mt-2 space-x-3 max-w-xs">
+<div class="flex-shrink-0 h-10 w-10 rounded-full bg-gray-300"></div>
+<div>
+    <div class="bg-gray-300 p-3 rounded-r-lg rounded-bl-lg">
+        <p class="text-sm">${message}</p>
+    </div>
+    <span class="text-xs text-gray-500 leading-none">${user}</span>
+</div>
+</div>
+`;
 
 const getUser = () => {
     const user = prompt('Enter username');
@@ -39,17 +51,25 @@ const reloadMessages = async () => {
     const m = await getMessages();
     messages.innerHTML = '';
     for (const [key, value] of Object.entries(m)) {
-        messages.innerHTML += `<p>${value.user}:${value.message}`;
+        messages.innerHTML += messageCard(value.user, value.message);
       }
     console.log(m);
 }
 
 
 
-send.addEventListener('click', (e) => {
-    addMessage(username, in1.value).then(() => {
-        reloadMessages();
-    })
+in1.addEventListener('keypress', (e) => {
+    if (e.key === "Enter") {
+        // Cancel the default action, if needed
+        e.preventDefault();
+        addMessage(username, in1.value).then(() => {
+            reloadMessages().then(() => {
+                document.getElementById('messages').scrollIntoView({ behavior: 'smooth', block: 'end' });
+                in1.value = '';
+            })
+        });
+      }
+    
 })
 
 
